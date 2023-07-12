f91aae7752f56f8e49d2f7f5d1d971025fe0859a
Felix Niemeyer <niemeyer.felix@gmail.com>
2023-07-13 01:28:39 +0200

    render a single midi file

--- generate-songs-with-elements.py
+++ generate-songs-with-elements.py
@@ -2,17 +2,18 @@ import os
 import mido
 import subprocess
 
-import time
-
 import argparse 
 
 parser = argparse.ArgumentParser(description='Generate songs with elements')
 parser.add_argument('--midi-path', type=str, default='lakh-midi-dataset-clean', help='path to the midi files')
+parser.add_argument('--single-file', type=str, default=None, help='path to a single midi file instead of --midi-path')
+
 parser.add_argument('--soundfont-path', type=str, default='soundfonts', help='path to the soundfonts')
 parser.add_argument('--out-path', type=str, default='rendered-midi', help='path to the output directory')
 
 parser.add_argument('--sample-rate', type=int, default=44100, help='sample rate')
 parser.add_argument('--bits', type=int, default=16, help='bits')
+parser.add_argument('--max-song-length', type=int, default=900, help='max song length in seconds')
 
 # parse arguments
 args = parser.parse_args()
@@ -21,132 +22,115 @@ sample_rate = args.sample_rate
 bits = args.bits
 channels = 2
 
+class Soundfont: 
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+
 # read all available soundfonts in the soundfonts folder
 print('Loading soundfonts from ' + args.soundfont_path)
-soundfontnames = []
 soundfonts = []
 for i, soundfont in enumerate(os.listdir(args.soundfont_path)):
     if soundfont[-4:] == '.sf2':
         name = soundfont[0:-4]
         print(str(i + 1) + ". " + name)
-        soundfontnames.append(name)
-        soundfonts.append(soundfont)
+        path = os.path.join(args.soundfont_path, soundfont)
+        soundfonts.append(Soundfont(name, path))
 
 # mkdir out-path if it doesn't exist
 if not os.path.exists(args.out_path):
     os.makedirs(args.out_path, exist_ok=True)
 
+def generate_song(midifile, outpath, soundfonts): 
 
-print('\nRendering songs')
-# for every song in every artist dir
-count = 0
-skip = 32
-for artist in os.listdir(args.midi_path):
-    # continue if not a directory
-    if not os.path.isdir(args.midi_path + '/' + artist):
-        continue
-    for song in os.listdir(args.midi_path + '/' + artist):
-        # just for reducing the dataset size
-        if skip > 0: 
-            skip -= 1
-            continue
-        skip = 100
-        count += 1
+    try: 
+        mid = mido.MidiFile(midifile)
+        duration = mid.length
 
-        def song_log(msg):
-            print('\t' + msg) 
+        print(f'Song length: {duration:.2f} seconds')
+        if mid.length > args.max_song_length: # skip songs longer than 15 minutes. 
+            print('Skipping song because it is too long: ' + str(mid.length))
+            return 
 
-        try: 
-            midifile = args.midi_path + '/' + artist + '/' + song
-            print(str(count) + '. processing ' + artist + '/' + song) 
+        print(f'Track count: {len(mid.tracks)}')
 
-            mid = mido.MidiFile(midifile)
-            duration = mid.length
+        # analyze midi file and find all kicks and snares
+        kicks = []
+        snares = []
 
-            print(f'Song length: {duration:.2f} seconds')
-            if mid.length > 900: # skip songs longer than 15 minutes. 
-                print('Skipping song because it is too long: ' + str(mid.length))
-                continue
+        hasRelevantPercussion = False
 
-            # analyze midi file and find all kicks and snares
-            kicks = []
-            snares = []
 
-            hasRelevantPercussion = False
-            hasBrokenTracks = False
+        for i, track in enumerate(mid.tracks):
+            # look for program change to drums
+            ticks_per_beat = mid.ticks_per_beat
 
-            song_log(str(len(mid.tracks)) + ' tracks') 
-            for i, track in enumerate(mid.tracks):
-                # look for program change to drums
-                ticks_per_beat = mid.ticks_per_beat
+            trackKicks = []
+            trackSnares = []
+            tempo = 500000 
+            ticks = 0
 
-                trackKicks = []
-                trackSnares = []
-                tempo = 500000 
-                ticks = 0
+            accumulated_time = 0
 
-                accumulated_time = 0
+            using_default_tempo = True
+            tempo_changed = False
 
-                using_default_tempo = True
-                tempo_changed = False
+            for msg in track:
+                ticks += msg.time
+                if msg.type == 'set_tempo':
+                    accumulated_time += mido.tick2second(ticks, ticks_per_beat, tempo)
+                    if using_default_tempo: 
+                        using_default_tempo = False
+                    elif tempo != msg.tempo:
+                        tempo_changed = True
+                    tempo = msg.tempo
+                    ticks = 0
+                time = accumulated_time + mido.tick2second(ticks, ticks_per_beat, tempo)
+                if msg.type == 'note_on': 
+                    if msg.channel == 9:
+                        if msg.note == 36 or msg.note == 35:
+                            trackKicks.append(time) 
+                        if msg.note >= 37 and msg.note <= 40:
+                            trackSnares.append(time)
 
-                for msg in track:
-                    ticks += msg.time
-                    if msg.type == 'set_tempo':
-                        accumulated_time += mido.tick2second(ticks, ticks_per_beat, tempo)
-                        if using_default_tempo: 
-                            using_default_tempo = False
-                        elif tempo != msg.tempo:
-                            tempo_changed = True
-                        tempo = msg.tempo
-                        ticks = 0
-                    time = accumulated_time + mido.tick2second(ticks, ticks_per_beat, tempo)
-                    if msg.type == 'note_on': 
-                        if msg.channel == 9:
-                            if msg.note == 36 or msg.note == 35:
-                                trackKicks.append(time) 
-                            if msg.note >= 37 and msg.note <= 40:
-                                trackSnares.append(time)
+            if(len(trackKicks) > 10 or len(trackSnares) > 10):
+                hasRelevantPercussion = True
+                print('Found drums in track ' + str(i) + ' ' + track.name)
+                kicks += trackKicks
+                snares += trackSnares
 
-                if(len(trackKicks) > 10 or len(trackSnares) > 10):
-                    hasRelevantPercussion = True
-                    song_log('Found drums in track ' + str(i) + ' ' + track.name)
-                    kicks += trackKicks
-                    snares += trackSnares
+            if tempo_changed:
+                print('tempo in this song changes') 
 
-                if tempo_changed:
-                    song_log('tempo in this song changes') 
+        if not hasRelevantPercussion: 
+            print('Skipping ' + artist + '/' + song + ' because it has no relevant percussion')
+        else:
+            if not os.path.exists(outpath):
+                os.makedirs(outpath, exist_ok=True)
 
-            if not hasRelevantPercussion: 
-                song_log('Skipping ' + artist + '/' + song + ' because it has no relevant percussion')
-            else:
-                songname = song[0:-4]
-                outpath = args.out_path + '/' + artist + '/' + songname + '/'
-                if not os.path.exists(outpath):
-                    os.makedirs(outpath, exist_ok=True)
+            # sort kicks and snares by time
+            kicks.sort()
+            snares.sort()
 
-                # sort kicks and snares by time
-                kicks.sort()
-                snares.sort()
+            # write kick timestamps to a file
+            kicksfile = os.path.join(outpath, 'kicks.txt')
+            with open(kicksfile, 'w') as f:
+                for msg in kicks:
+                    f.write(str(msg) + '\n')
+            # write snare timestamps to a file
+            snaresfile = os.path.join(outpath, 'snares.txt')
+            with open(snaresfile, 'w') as f:
+                for msg in snares:
+                    f.write(str(msg) + '\n')
 
-                # write kick timestamps to a file
-                with open(outpath + 'kicks.txt', 'w') as f:
-                    for msg in kicks:
-                        f.write(str(msg) + '\n')
-                # write snare timestamps to a file
-                with open(outpath + 'snares.txt', 'w') as f:
-                    for msg in snares:
-                        f.write(str(msg) + '\n')
-
-                # for i in range(len(synths)):
-                font = count % len(soundfontnames) # reduce data size
-                outfile = outpath + soundfontnames[font] + '.wav'
+            for soundfont in soundfonts: 
+                outfile = os.path.join(outpath, soundfont.name + '.wav')
 
                 # check if file already exists 
                 if os.path.exists(outfile):
-                    song_log('Skipping ' + artist + '/' + song + ' because it already exists')
+                    print('Skipping ' + artist + '/' + song + ' because it already exists')
                 else: 
-                    song_log('Rendering midi using soundfont ' + soundfontnames[font])
+                    print('Rendering midi using soundfont ' + soundfont.name)
                     p = subprocess.Popen([
                         'fluidsynth',
                         '-F', outfile, 
@@ -155,33 +139,57 @@ for artist in os.listdir(args.midi_path):
                         '-g', '1', 
                         '-O', f's{bits}', 
                         '-r', str(sample_rate),
-                        f'{args.soundfont_path}/{soundfonts[font]}',
+                        soundfont.path,
                         midifile,
                     ], stderr=subprocess.DEVNULL) # muting errors
-                    max_size = (duration + 1) * sample_rate * channels * bits / 8 
+                    max_size = (duration + 10) * sample_rate * channels * bits / 8 
                     max_size += 1024 * 1024 # add 1MB to the max size
                     while p.poll() is None:
                         #if outfile exists
                         if os.path.exists(outfile): 
                             if os.stat(outfile).st_size > max_size:
-                                song_log('File size bigger than expected, possibly a corrupt midi file')
-                                song_log('Stopping fluidsynth...')
+                                print('File size bigger than expected, possibly a corrupt midi file')
+                                print('Stopping fluidsynth...')
                                 p.kill()
                                 p.wait()
                                 # remove file
-                                song_log('Removing file')
+                                print('Removing file')
                                 os.remove(outfile)
                                 raise Exception('File size bigger than expected, possibly a corrupt midi file')
 
-                    p.wait()
+                    # p.wait() # equivalent of 'while p.poll() is None'?
 
                     if p.returncode != 0:
-                        song_log('Fluidsynth returned non-zero exit code')
+                        print('Fluidsynth returned non-zero exit code')
                         os.remove(outfile) # if an error occured, it's better to remove the file
                         raise Exception('Fluidsynth returned non-zero exit code')
 
-        except Exception as e:
-            song_log('Error processing ' + artist + '/' + song)
-            song_log(str(e))
-            print()
+    except Exception as e:
+        print('Error processing ' + artist + '/' + song)
+        print(str(e))
 
+
+if args.single_file != None:
+    print('Rendering single song' + args.single_file)
+    generate_song(args.single_file, args.out_path, soundfonts)
+    exit(0)
+
+else: 
+    print('Rendering songs from ' + args.midi_path)
+    # for every song in every artist dir
+    count = 0
+    for artist in os.listdir(args.midi_path):
+        # continue if not a directory
+        if not os.path.isdir(args.midi_path + '/' + artist):
+            continue
+        for song in os.listdir(args.midi_path + '/' + artist):
+            midifile = args.midi_path + '/' + artist + '/' + song
+            print('\n\n') 
+            print(str(count) + '. processing ' + artist + '/' + song) 
+
+            songname = song[0:-4]
+            outpath = args.out_path + '/' + artist + '/' + songname + '/'
+
+            generate_song(midifile, outpath, [soundfonts[count % len(soundfonts)]])
+
+            count += 1
---
+++ view-audio-and-timestamps.py
@@ -0,0 +1,72 @@
+# this visualizes one chunk's wav and the kicks and snare positions from the respective files
+import os
+import argparse
+
+import soundfile as sf
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser(
+    'audio and timestamps viewer',
+    'View a chunk of audio and its kicks and snares', 
+    'Either provide a path to a chunk (--chunk) or to a rendered midi (--rendered-midi)'
+) 
+
+parser.add_argument('--chunk', help='chunk to view. timestamp files are expected to be named the same as the .opus audio file but with file endings .kicks or .snares respectively', type=str)
+parser.add_argument('--rendered-midi', help='rendered midi to view. timestamp files are expected to be in the same directory and named kicks.txt or snares.txt respectively') 
+
+args = parser.parse_args()
+
+if args.chunk is not None:
+    # strip .opus from chunk name if present
+    if args.chunk.endswith('.opus'):
+        args.chunk = args.chunk[:-5]
+    audiofile = args.chunk + '.opus'
+    kickfile = args.chunk + '.kicks'
+    snarefile = args.chunk + '.snares'
+elif args.rendered_midi is not None:
+    audiofile = args.rendered_midi
+    # get the directory of the rendered midi
+    directory = os.path.dirname(args.rendered_midi)
+    kickfile = os.path.join(directory, 'kicks.txt')
+    snarefile = os.path.join(directory, 'snares.txt')
+else: 
+    print('No file provided. Provide either a file either with --chunk or --rendered-midi') 
+    parser.print_help()
+    exit(0)
+
+audio_data, samplerate = sf.read(audiofile) 
+
+if len(audio_data.shape) > 1: 
+    audio_data = audio_data[:, 0]
+
+duration = len(audio_data) / samplerate
+
+print('samplerate', samplerate) 
+
+# Create time array
+time = np.linspace(0, duration, num=len(audio_data))
+
+# Plot WAV file
+plt.plot(time, audio_data, color='black', linewidth=0.1)
+
+# Mark kicks on the time axis
+with open(kickfile, 'r') as file:
+    for line in file: 
+        plt.axvline(x=float(line), ymax=0.5, color='r', linestyle='--')
+
+# Mark snares on the time axis
+with open(snarefile, 'r') as file:
+    for line in file: 
+        plt.axvline(x=float(line), ymax=0.5, color='g', linestyle='--')
+
+# Set labels and title
+plt.xlabel('Time [s]')
+plt.ylabel('Amplitude')
+plt.title('Chunk with kicks (red) and snares (green)')
+
+# Display the plot
+plt.show()
+
